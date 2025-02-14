1c3c564772eefa28d321fc49a27beddcfdf2d117
Clayton Sweeten <cjsweeten101@gmail.com>
2025-02-14 14:36:22 -0700

    knight finds shortest path

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 # Responsible for finding minimum paths on a chess board
 class Knight
   attr_accessor :grid_size
@@ -8,22 +10,39 @@ class Knight
     @grid_size = 7
   end
 
-  # Idea: from the starting arr, find all possible moves
-  # ie children in graph, push them on to queue
-  # and iterate through queue and do the same
-  # and make sure to keep track of visited spaces
   def knight_moves(from, to)
     queue = [from]
-    visited_spaces [from]
+    parent = {}
     current_space = nil
     until current_space == to
-      current_space queue.shift
-      find_next_moves(current_space).each { |m| queue << m }
+      current_space = queue.shift
+      get_next_moves(current_space).each do |space|
+        parent[space] = current_space if parent[space].nil?
+        queue << space
+      end
+    end
+    print_path(get_path(from, to, parent))
+  end
+
+  def get_path(from, to, parents)
+    current_parent = parents[to]
+    result = [current_parent]
+    until current_parent == from
+      current_parent = parents[current_parent]
+      result << current_parent
+    end
+    result.reverse << to
+  end
+
+  def print_path(path)
+    puts "You made it in #{path.size - 1} moves! Here's your path:"
+    path.each do |pos|
+      p pos
     end
   end
 
   # TODO: Find a way to do this cleaner
-  def find_next_moves(pos)
+  def get_next_moves(pos)
     result = []
 
     result << [pos[0] + 2, pos[1] + 1] if valid?([pos[0] + 2, pos[1] + 1])
@@ -38,7 +57,6 @@ class Knight
   end
 
   def valid?(pos)
-    p pos
     pos.none? { |element| element > @grid_size || element.negative? }
   end
 end
